12648729ed07d64734ec7151ba0f533e4b2952e7
NakshatraCodes <nakshatra163@gmail.com>
2020-05-12 22:52:58 +0530

    Test

--- scrape.js
+++ scrape.js
@@ -1,15 +1,16 @@
 const cheerio = require('cheerio');
 const axios = require('axios');
 
-async function getHTML(){
-	const {data: html} = await axios.get('https://www.google.com/search?q=automate the boring stuff with python');
-	return html;
-}
+axios.get('https://www.google.com/search?q=automate')
+    .then((response) => {
+        if(response.status === 200) {
+            const html = response.data;
+            const $ = cheerio.load(html); 
+           	$('div#main div#cnt div.mw div#rcnt div.col div#centre_col div.med div#search div div div.g').each((i, elem)=>{
+           		console.log(elem);
+           	});
+    	}
+}, (error) => console.log(err) );
 
-async function loadCheerio(html){
-	const $ = cheerio.load(html);
-	const span = $.html();
-	console.log(span);
-}
 
-loadCheerio(getHTML());
+// getHTML();
