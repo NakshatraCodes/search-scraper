9df10081ce8d930d35bc48577086c15b2c84e96b
NakshatraCodes <nakshatra163@gmail.com>
2020-05-12 21:15:41 +0530

    Saving changes

--- app.js
+++ app.js
@@ -7,6 +7,26 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
+
+var scraper = require('google-search-scraper');
+ 
+var options = {
+  query: 'nodejs',
+  limit: 10
+};
+ 
+scraper.search(options, function(err, url, meta) {
+  // This is called for each result
+  if(err) throw err;
+  console.log(url);
+  console.log(meta.title);
+  console.log(meta.meta);
+  console.log(meta.desc)
+});
+
+
+// Routes
+
 const PORT = 3000;
 
 app.get("/", function(req, res){
@@ -16,3 +36,6 @@ app.get("/", function(req, res){
 app.listen(PORT, ()=>{
 	console.log(`Server is listening at PORT:${PORT}`);
 });
+
+// Package Functions
+
